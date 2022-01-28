905bf947f9f31fb86c5c4282c14ade66c878d668
Rich Wareham <rich@richwareham.com>
2022-01-28 11:33:24 +0000

    update example to be a webapp with a blank page

---
+++ src/client/index-page/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndexPage from './components/IndexPage';
+
+import './styles.css';
+
+ReactDOM.render(<IndexPage />, document.getElementById('index'));
